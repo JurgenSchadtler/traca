594fe28386113dd79a998106b40e6ece66450a87
Jürgen Schädtler <jschadtlerz@gmail.com>
2022-03-23 10:55:31 -0600

    update icons unidades with fancybox

--- js/main.js
+++ js/main.js
@@ -25,3 +25,21 @@ const sr = ScrollReveal({
     origin:'bottom',
     interval:100,
   })
+
+
+  $('[data-fancybox]').fancybox({
+    // Options will go here
+    buttons : [
+      'close'
+    ],
+    wheel : false,
+    transitionEffect: "slide",
+     // thumbs          : false,
+    // hash            : false,
+    loop            : true,
+    // keyboard        : true,
+    toolbar         : false,
+    // animationEffect : false,
+    // arrows          : true,
+    clickContent    : false
+  });
